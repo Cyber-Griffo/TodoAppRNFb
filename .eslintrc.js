6750530b51ba39607bc4c0e3dd22c9b65e9af4f3
Cyber-Griffo <christoph.gerling1603@gmail.com>
2022-08-29 10:31:50 +0200

    added lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'unused-imports'],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
@@ -10,7 +10,15 @@ module.exports = {
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
         'no-undef': 'off',
+        'sort-imports': [
+          'error',
+          {
+            ignoreDeclarationSort: true,
+          },
+        ],
+        'unused-imports/no-unused-imports': 'error',
+        semi: ['warn', 'never'],
       },
     },
   ],
-};
+}
